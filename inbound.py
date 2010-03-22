fc830b0caf29fe1424bc8fe30afcf7e21d8ecd72
Alex MacCaw <maccman@gmail.com>
2010-03-22 13:13:34 +0000

    Raise if response is not 200

--- inbound.py
+++ inbound.py
@@ -3,20 +3,25 @@ from django.utils import simplejson as json
 from google.appengine.ext import webapp, deferred
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api.urlfetch import fetch
+from google.appengine.api.urlfetch import Error as FetchError
 
 settings = yaml.load(open('settings.yaml'))
 
 def callback(raw):
   result = {'email': {'raw': raw}}
   
-  fetch(settings['outbound_url'], 
-        payload=json.dumps(result), 
-        method="POST", 
-        headers={
-          'Authorization': settings['api_key'],
-          'Content-Type': 'application/json'
-        }
-       )
+  response = fetch(settings['outbound_url'], 
+              payload=json.dumps(result), 
+              method="POST", 
+              headers={
+                'Authorization': settings['api_key'],
+                'Content-Type': 'application/json'
+              },
+              deadline=10
+             )
+  logging.info(response.status_code)
+  if response.status_code != 200:
+    raise FetchError()
 
 class InboundHandler(InboundMailHandler):
     def receive(self, message):
